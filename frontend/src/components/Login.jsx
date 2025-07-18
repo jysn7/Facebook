8fcc9f1fade1651183820d3d6f208a9a8003b22a
Jayson Baloyi <jysnbly7@gmail.com>
2025-07-18 21:49:15 +0200

    face

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -66,6 +66,9 @@ const Login = () => {
         >
           Sign In
         </button>
+        <p className="text-xs text-gray-500 mt-2">
+          This app is not affiliated with or endorsed by Facebook.
+        </p>
       </div>
     </div>
   );
